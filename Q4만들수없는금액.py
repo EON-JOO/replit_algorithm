82404934ac5c0154cfc4c31446b0d451dc47a9a3
EON-JOO <svtr357@naver.com>
2022-05-30 04:27:00 +0000

    greedy

---
+++ Q4만들수없는금액.py
@@ -0,0 +1,20 @@
+# 2022-05-30 4:04 ~ 4:23
+n = int(input())
+coins = list(map(int, input().split()))
+
+max_val = sum(coins)
+coins.sort()
+val = []
+
+for i in range(len(coins) - 1):
+  for j in range(i + 1, len(coins)):
+    val.append(sum(coins[i:j+1]))
+
+val.sort()
+
+for i in range(len(val) - 1):
+  if val[i] + 1 < val[i+1]:
+    res = val[i] + 1
+    break
+
+print(res)
