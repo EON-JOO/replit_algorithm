c4c2430f9b40aeb5d6fe92fd075eafc6133f0876
EON-JOO <svtr357@naver.com>
2022-06-02 16:08:10 +0000

    programmers greedy

--- 03_Greedy/[PG]조이스틱.py
+++ 03_Greedy/[PG]조이스틱.py
@@ -2,6 +2,8 @@
 # 채점 결과
 # 정확성: 63.0
 # 합계: 63.0 / 100.0
+# 일단 그리디로 안 풀었고, 테스트케이스 추가되어서 그리디로 안되고 완전탐색(Brute Force)으로 풀림 -> 추후 다시 풀이 시도
+# https://velog.io/@jqdjhy/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%A1%B0%EC%9D%B4%EC%8A%A4%ED%8B%B1-Greedy
 
 def solution(name):
     answer = 0
